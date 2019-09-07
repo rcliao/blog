e36bc623332643d6d75187925cb974177dabd955
Eric Liao <rcliao01@gmail.com>
2019-09-07 16:52:30 +0000

    refactor(script): rm public folder before deploy

--- script/deploy.sh
+++ script/deploy.sh
@@ -3,6 +3,8 @@
 # If a command fails then the deploy stops
 set -e
 
+rm -rf public
+
 git submodule add -b master git@github.com:rcliao/rcliao.github.io.git public
 
 printf "\033[0;32mDeploying updates to GitHub...\033[0m\n"
